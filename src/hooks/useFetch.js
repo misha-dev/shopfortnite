7e810290722274f0ff359dff529abeaa0bb4cc8c
Michael <michaelshabatin@mail.ru>
2022-05-25 23:41:26 +0500

    added  config

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-
-const useFetch = (link) => {
+import { API_KEY, API_URL } from "../config/config";
+export const useFetch = () => {
   const [data, setData] = useState();
   const [error, setError] = useState();
   const [isLoading, setIsLoading] = useState(false);
@@ -8,8 +8,13 @@ const useFetch = (link) => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch("link");
+        const response = await fetch(API_URL, {
+          headers: {
+            Authorization: API_KEY,
+          },
+        });
         const data = await response.json();
+        console.log(data);
         setData(data);
       } catch (error) {
         setError(error.message);
