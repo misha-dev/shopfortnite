b376ccb10f0cf4f9f4c73c38fe6dc583ea58abeb
Misha-dev <shabatinmichael@mail.ru>
2022-06-19 13:44:41 +0500

    refactored code with immer.js: state in context is immutable now

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { createContext, useEffect, useReducer } from "react";
 
 export const CartContext = createContext(null);
@@ -13,96 +14,68 @@ export const CartContextProvider = ({ children }) => {
       return -1;
     };
     const index = findItem(action.data.mainId);
-    switch (action.type) {
-      case "GETCONTEXT":
+    return produce(state, (draft) => {
+      if (action.type === "GETCONTEXT") {
         const localState = localStorage.getItem("context");
+        draft.cart = [];
+        draft.totalCount = 0;
+        draft.totalPrice = 0;
         if (localState) {
-          const localStateParsed = JSON.parse(localState);
-          localStateParsed.context = true;
-          return { ...localStateParsed };
+          const parsedContext = JSON.parse(localState);
+          draft.cart = [...parsedContext.cart];
+          draft.totalCount = parsedContext.totalCount;
+          draft.totalPrice = parsedContext.totalPrice;
+          draft.context = true;
+        } else {
+          draft.context = true;
+          localStorage.setItem("context", JSON.stringify(draft));
         }
-        state.context = true;
-        localStorage.setItem("context", JSON.stringify(state));
-        console.log(state);
-        return { ...state };
-
-      case "ADD":
+      } else if (action.type === "ADD") {
         if (index === -1) {
           action.data.count = 1;
-          const newState = {
-            cart: [action.data, ...state.cart],
-            totalCount: state.totalCount + 1,
-            totalPrice: state.totalPrice + action.data.price,
-            context: true,
-          };
-          localStorage.setItem("context", JSON.stringify(newState));
-          return newState;
+          draft.cart.push(action.data);
+          draft.totalCount++;
+          draft.totalPrice += action.data.price;
+          localStorage.setItem("context", JSON.stringify(draft));
         } else {
-          state.cart[index].count += 1;
-          const newState = {
-            ...state,
-            totalCount: state.totalCount + 1,
-            totalPrice: state.totalPrice + action.data.price,
-            context: true,
-          };
+          draft.cart[index].count += 1;
+          draft.totalCount++;
+          draft.totalPrice += action.data.price;
 
-          localStorage.setItem("context", JSON.stringify(newState));
-
-          return newState;
+          localStorage.setItem("context", JSON.stringify(draft));
         }
-
-      case "DECREASE":
+      } else if (action.type === "DECREASE") {
         if (state.cart[index].count === 1) {
-          state.cart = state.cart.filter((item) => {
+          draft.cart = state.cart.filter((item) => {
             return item.mainId !== action.data.mainId;
           });
         } else {
-          state.cart[index].count -= 1;
+          draft.cart[index].count -= 1;
         }
 
-        const newState = {
-          ...state,
-          totalCount: state.totalCount - 1,
-          totalPrice: state.totalPrice - action.data.price,
-          context: true,
-        };
+        draft.totalCount--;
+        draft.totalPrice -= action.data.price;
 
-        localStorage.setItem("context", JSON.stringify(newState));
-
-        return newState;
-
-      case "DELETE":
-        state.cart = state.cart.filter((item) => {
+        localStorage.setItem("context", JSON.stringify(draft));
+      } else if (action.type === "DELETE") {
+        draft.cart = state.cart.filter((item) => {
           return item.mainId !== action.data.mainId;
         });
 
-        const newStateDel = {
-          ...state,
-          totalCount: state.totalCount - action.data.count,
-          totalPrice: state.totalPrice - action.data.price * action.data.count,
-          context: true,
-        };
+        draft.totalCount -= action.data.count;
+        draft.totalPrice -= action.data.price * action.data.count;
 
-        localStorage.setItem("context", JSON.stringify(newStateDel));
-
-        return newStateDel;
-
-      case "CLEAR":
-        const newStateClear = {
-          cart: [],
-          totalCount: 0,
-          totalPrice: 0,
-          context: true,
-        };
-        localStorage.setItem("context", JSON.stringify(newStateClear));
-
-        return newStateClear;
-
-      default:
-        return state;
-    }
+        localStorage.setItem("context", JSON.stringify(draft));
+      } else if (action.type === "CLEAR") {
+        draft.cart = [];
+        draft.totalCount = 0;
+        draft.totalPrice = 0;
+        localStorage.setItem("context", JSON.stringify(draft));
+      }
+    });
   };
   const [state, dispatch] = useReducer(cartReducer, {
+    // @ts-ignore
     cart: [],
     totalCount: 0,
     totalPrice: 0,
@@ -118,7 +91,10 @@ export const CartContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <CartContext.Provider value={{ ...state, dispatch }}>
+    <CartContext.Provider
+      // @ts-ignore
+      value={{ ...state, dispatch }}
+    >
       {children}
     </CartContext.Provider>
   );
